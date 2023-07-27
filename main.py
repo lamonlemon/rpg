7b0459141f5747ce6032c319d2f6e837c3d8aae8
lamonlemon <jw.molang@gmail.com>
2023-07-27 18:44:54 +0900

    commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+wood = 0
 
 @app.route('/')
 def index():
-    return render_template("index.html",woods = '1')
+    global wood
+    return render_template("index.html",woods = wood)
+
+@app.route('/',methods=['POST'])
+def wood_add():
+    global wood
+    wood = wood + 1
+    return render_template("index.html",woods = wood)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
