b125d503258a92e4ac3e381fc302b8aa48f5da7c
lamonlemon <jw.molang@gmail.com>
2023-07-27 17:56:46 +0900

    commit

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-woods = 1
-
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",woods = '1')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
