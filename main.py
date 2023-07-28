c958ae151c22811bc967a44be7b868c3a251c12e
lamonlemon <jw.molang@gmail.com>
2023-07-28 16:26:31 +0900

    commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-wood = 0
+wood = 100000
 tool = 'wood axe'
 need = 0
 
@@ -11,39 +11,22 @@ def index():
     global tool
     return render_template("index.html",woods = wood,tool = tool)
 
-@app.route('/',methods=['POST'])
-def wood_add():
-    global wood
-    wood = wood + 1
-    return jsonify({'wood': wood})
-
-@app.route('/make_house', methods=['POST'])
-def decrement_counter():
+@app.route('/', methods=['POST'])
+def modify_wood():
     global wood
     global need
-    need += 10
-    wood -= need  # 나무 10개씩 빼기
-    if wood < 0:
-        wood = 0
-    return jsonify({'wood': wood},{'need':need})
-@app.route('/save', methods=['POST'])
-def save_data():
-    global wood
-    data = request.form.get('data')
-    # 데이터 저장
-    with open('data.txt', 'w') as file:
-        file.write(data)
-    return jsonify({'message': '저장 완료'})
-
-@app.route('/load', methods=['GET'])
-def load_data():
-    # 데이터 불러오기
-    try:
-        with open('data.txt', 'r') as file:
-            data = file.read()
-        return jsonify({'data': data})
-    except FileNotFoundError:
-        return jsonify({'data': ''})
+    if request.method == 'POST':
+        action = request.form.get('action')
+        if action == 'add':
+            wood += 1
+        elif action == 'subtract':
+            wood -= need
+            need += 10
+            if wood < 0:
+                wood = 0
+            if need >= 100:
+                return jsonify({'wood': wood, 'need': 'Max'})
+    return jsonify({'wood': wood, 'need': need})
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -1,71 +1,53 @@
 var chop = 100;
-const drop = document.querySelector("#drop");
-drop.disabled = true;
-document.getElementById('incrementBtn').addEventListener('click', function() {
+// document.getElementById('build').textContent = 10;
+
+function modifyWood(action) {
     // AJAX를 이용하여 서버로 데이터 전송
-    fetch('', {
+    fetch('/', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'action=' + action,
     })
     .then(response => response.json())
     .then(data => {
         // 서버에서 받은 데이터로 화면 업데이트
         document.getElementById('wood').textContent = data.wood;
-        if(data.wood >= 10){
-            drop.disabled = false;
-        }else {
-            drop.disabled = true;
+        if (data.need == '') {
+            data.need = 10; // need가 빈 문자열일 때 10으로 설정
         }
+        
+        document.getElementById('build').textContent = data.need;
+        // 나무 개수가 10개 이상인 경우 빼기 버튼 활성화, 아닌 경우 비활성화
+        if(typeof data.need == 'number'){
+            document.getElementById('decrementBtn').disabled = data.wood < data.need;
+        }else{
+            document.getElementById('decrementBtn').disabled = true;
+        }
+        // 로컬 스토리지에 나무 개수 저장
+        localStorage.setItem('wood', data.wood);
+        localStorage.setItem('need', data.need);
     })
     .catch(error => console.error('Error:', error));
+
     document.getElementById('incrementBtn').disabled = true;
     setTimeout(function() {
         document.getElementById('incrementBtn').disabled = false;
     }, chop);
-});
-document.getElementById('drop').addEventListener('click', function() {
-    // AJAX를 이용하여 서버로 데이터 전송
-    fetch('/make_house', {
-        method: 'POST',
-    })
-    .then(response => response.json())
-    .then(data => {
-        // 서버에서 받은 데이터로 화면 업데이트
-        document.getElementById('wood').textContent = data.wood;
-        document.getElementById('need').textContent = data.need;
-        if(data.wood >= data.need){
-            drop.disabled = false;
-        }else {
-            drop.disabled = true;
-        }
-    })
-    .catch(error => console.error('Error:', error));
-});
-document.getElementById('saveBtn').addEventListener('click', function() {
-    var data = prompt('저장할 데이터를 입력하세요:');
-    if (data !== null) {
-        // AJAX를 이용하여 서버로 데이터 전송
-        fetch('/save', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: 'data=' + encodeURIComponent(data),
-        })
-        .then(response => response.json())
-        .then(data => {
-            alert(data.message);
-        })
-        .catch(error => console.error('Error:', error));
-    }
-});
+}
 
-document.getElementById('loadBtn').addEventListener('click', function() {
-    // AJAX를 이용하여 서버로 데이터 요청
-    fetch('/load')
-    .then(response => response.json())
-    .then(data => {
-        // 서버에서 받은 데이터로 화면 업데이트
-        document.getElementById('dataContainer').textContent = '불러온 데이터: ' + data.data;
-    })
-    .catch(error => console.error('Error:', error));
+// 페이지 로드 시 로컬 스토리지에 저장된 데이터 로드
+window.addEventListener('load', function() {
+    var storedWood = localStorage.getItem('wood');
+    var storedNeed = localStorage.getItem('need');
+    if (storedWood !== null) {
+        document.getElementById('wood').textContent = storedWood;
+    }
+    if (storedNeed !== null){
+        if(storedNeed === 'Max'){
+            document.getElementById('decrementBtn').disabled = true;
+        }
+        document.getElementById('build').textContent = storedNeed;
+    }
 });
